d14ffb96bc24cb44ef547e925a8ef395f9ce09ea
KN <kncxstudio@gmail.com>
2018-11-08 17:18:25 +0800

    sync

--- client/client.go
+++ client/client.go
@@ -1,22 +1,41 @@
 package main
 
 import (
-	."EverySync/util"
+	"EverySync/pb"
+	. "EverySync/util"
+	"github.com/golang/protobuf/proto"
 	"github.com/xtaci/kcp-go"
 	"log"
-	"time"
 )
 
-const ServerAddr = "localhost:13301"
+const ServerAddr = "149.28.59.218:13301"
 func main(){
 	clientSess, err := kcp.DialWithOptions(ServerAddr, nil, 10, 3)
 	CheckErr(err)
 	log.Println("Client connected to ", ServerAddr)
 
-	for {
-		_, err := clientSess.Write([]byte("clientSess says hello!"))
-		CheckErr(err)
-		log.Println("client write success!")
-		time.Sleep(1 * time.Second)
+
+	client := &pb.Client{
+		ID: "asdadawfgd",
+		Addr: "",
 	}
+
+	data, _ := proto.Marshal(client)
+	clientSess.Write(data)
+
+
+	data = make([]byte, 1024)
+	n, err := clientSess.Read(data)
+	CheckErr(err)
+	otherClient := &pb.Client{}
+	unmarshalErr := proto.Unmarshal(data[:n], otherClient)
+	CheckErr(unmarshalErr)
+
+	log.Println(otherClient)
+	//for {
+	//	_, err := clientSess.Write([]byte("clientSess says hello!"))
+	//	CheckErr(err)
+	//	log.Println("client write success!")
+	//	time.Sleep(1 * time.Second)
+	//}
 }
--- server/server.go
+++ server/server.go
@@ -1,30 +1,81 @@
 package main
 
 import (
+	"EverySync/pb"
 	. "EverySync/util"
+	"github.com/golang/protobuf/proto"
+	"github.com/patrickmn/go-cache"
 	"github.com/xtaci/kcp-go"
 	"log"
 	"net"
+	"time"
 )
+
+var clientsCache = cache.New(5 * time.Minute, 10 * time.Minute)
+var addrsCache = cache.New(5 * time.Minute, 10 * time.Minute)
 
 func main(){
 	server, err := kcp.ListenWithOptions(":13301", nil, 10, 3)
 	CheckErr(err)
 	for {
-		client, err := server.Accept()
+		sess, err := server.Accept()
 		CheckErr(err)
-		go ReadDataFromClient(client)
+		go AddSessToCache(sess)
+	}
+}
+
+// AddSessToCache
+func AddSessToCache(sess net.Conn){
+	clientInfo := make([]byte,1024)
+	n, _ := sess.Read(clientInfo)
+	client := &pb.Client{}
+	unmarshalErr := proto.Unmarshal(clientInfo[:n], client)
+	CheckErr(unmarshalErr)
+	client.Addr = sess.RemoteAddr().String()
+
+	clientsCache.Set(sess.RemoteAddr().String(), client, cache.DefaultExpiration)
+	id := client.GetID()
+
+	temp, _ := addrsCache.Get(id)
+
+	var addrsList []string
+	if temp == nil {
+		addrsList = make([]string, 0)
+	}else {
+		addrsList = temp.([]string)
+	}
+
+	addrsList = append(addrsList, client.GetAddr())
+	addrsCache.Set(id, addrsList, cache.DefaultExpiration)
+
+	for _, addr := range addrsList{
+		log.Println("addr : ", addr)
+	}
+
+	if len(addrsList) > 1 {
+		log.Println("len : ", len(addrsList))
+		go SendPeers(sess, id)
 	}
 }
 
 
-func ReadDataFromClient(client net.Conn){
-	data := make([]byte, 1024)
 
-	for{
-		n, err := client.Read(data)
-		CheckErr(err)
-		log.Println("client msg:", string(data[:n]))
+func SendPeers(sess net.Conn, id string){
+	if addrs, found := addrsCache.Get(id); found {
+
+		for _, addr := range addrs.([]string){
+			if addr != sess.RemoteAddr().String(){
+				client := &pb.Client{
+					ID: id,
+					Addr: addr,
+				}
+				data, err := proto.Marshal(client)
+				CheckErr(err)
+				sess.Write(data)
+				break
+			}
+		}
+
 	}
 
 }
